57efb5720720255c80a89773327bee8e18cef2ab
cjcrist <cjc2@mail.usf.edu>
2016-10-28 15:51:50 -0400

    Add time and correct file name

--- ws-graph-gen.py
+++ ws-graph-gen.py
@@ -5,9 +5,10 @@ import sys
 import os
 import snap
 import math 
+import time
 
 file = sys.argv[1]
-filename = file[:-9] + "er" + file[-10:]
+filename = file[:-9] + "ws" + file[-10:]
 
 inGraph = snap.LoadEdgeList(snap.PUNGraph, file, 0, 1)
 nodes = inGraph.GetNodes()
@@ -20,7 +21,7 @@ print "nodes: %d, edges: %d" % (nodes, edges)
 
 path = os.path.join(r"p3_data", filename[3:])
 
-print "starting WS graph\n"
+print "starting WS graph at time %s\n" % time.ctime()
 edgeList = []
 outDegree = 0
 OutDegV = snap.TIntPrV()
@@ -42,4 +43,4 @@ for EI in smallWorldGraph.Edges():
 		for x in edgeList:
 			f.write('{0:4d} {1:9d}\n'.format(x[0], x[1]))
 
-print "finished WS graph\n"
+print "finished WS graph at time %s\n" % time.ctime()
